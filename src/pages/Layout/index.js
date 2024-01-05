f71f8830b51bd124d04c97a520a26421106b8ea4
zqh <1846508601@qq.com>
2024-01-05 11:32:21 +0800

    首次提交

---
+++ src/pages/Layout/index.js
@@ -0,0 +1,6 @@
+const Layout=()=>{
+    return (
+        <div>这是layout组件</div>
+    )
+}
+export default Layout
---
+++ src/pages/Login/index.js
@@ -0,0 +1,6 @@
+const Login = ()=>{
+    return (
+        <div>这是Login组件</div>
+    )
+}
+export default Login
